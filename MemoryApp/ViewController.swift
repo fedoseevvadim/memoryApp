c0b6fd242abb490571998308e2e6e3e99ce4fa70
test <test>
2019-09-07 21:35:02 +0300

    Initial Commit

---
+++ MemoryApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemoryApp
+//
+//  Created by Вадим Федосеев on 07/09/2019.
+//  Copyright © 2019 Вадим Федосеев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
